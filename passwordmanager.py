8038a7175262866469b5b9c6b67a9b9f592ecab3
Alex <93352449+alexoltl@users.noreply.github.com>
2022-04-26 17:37:25 +1200

    Add files via upload

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,22 +1,40 @@
-from colorama import Fore
+import base64
+import os
+import os.path
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from colorama import Fore
 
-'''
-def write_key():
-    key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
-        key_file.write(key)
-write_key()
-'''
+mpass = input("What is the master password? (this will be use to encrypt and decrypt other passwords, do not lose it):").encode()
 
-def load_key():
-    file = open("key.key", "rb")
-    key = file.read()
+def make_salt():
+    salt_exists = os.path.exists("salt.key")
+    while salt_exists:
+        break
+    else:
+        salt = os.urandom(16)
+        with open("salt.key", "wb") as key_file:
+            key_file.write(salt)
+make_salt()
+
+
+
+def load_salt():
+    file = open("salt.key", "rb")
+    salt = file.read()
     file.close()
-    return key 
+    return salt 
 
 
-key = load_key()
+salt = load_salt()
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=390000,
+)
+key = base64.urlsafe_b64encode(kdf.derive(mpass))
 fer = Fernet(key)
 
 
@@ -34,7 +52,6 @@ def add():
 
     with open("passwords.txt", "a") as f:
         f.write(f"{name}|{fer.encrypt(pwd.encode()).decode()}\n")
-
 
 while True:
     main = input(f"{Fore.BLUE}\nDo you want to view or add passwords?{Fore.GREEN}\n\n1. View\n\n2. Add\n\n3. Quit\n\n{Fore.RESET}>>> ")
